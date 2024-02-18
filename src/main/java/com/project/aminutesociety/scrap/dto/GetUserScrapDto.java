2ba42c0b6a092f53d02a47318be7f09545d80ff8
7beunseo <kimes0403@gmail.com>
2024-02-18 16:24:09 +0900

    ✨ Thumbnail 추가

--- src/main/java/com/project/aminutesociety/scrap/dto/GetUserScrapDto.java
+++ src/main/java/com/project/aminutesociety/scrap/dto/GetUserScrapDto.java
@@ -25,6 +25,7 @@ public class GetUserScrapDto {
             private String videoTitle;
             private String videoRunTime;
             private String url;
+            private String thumbnail;
 
             public static ScrapList EntityToDto(Scrap scrap) {
                 return ScrapList.builder()
@@ -32,6 +33,7 @@ public class GetUserScrapDto {
                         .videoId(scrap.getVideo().getId())
                         .videoTitle(scrap.getVideo().getTitle())
                         .url(scrap.getVideo().getUrl())
+                        .thumbnail(scrap.getVideo().getThumbnail())
                         .videoRunTime(formatRunTime(scrap.getVideo().getRunTime()))
                         .build();
             }
