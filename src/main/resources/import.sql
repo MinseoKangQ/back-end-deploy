754193fdf040384d000dddd5e40422ec981bd4b4
7beunseo <kimes0403@gmail.com>
2024-02-23 23:18:31 +0900

    🗃️ 더미데이터 변경

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,25 +1,56 @@
 INSERT INTO categories (category_name) VALUES ('IT');
-INSERT INTO categories (category_name) VALUES ('Economy/Finance');
-INSERT INTO categories (category_name) VALUES ('lifestylcategoriese');
-INSERT INTO categories (category_name) VALUES ('Cooking/Baking');
-INSERT INTO categories (category_name) VALUES ('Exercise/Fitness');
-INSERT INTO categories (category_name) VALUES ('Photo/Video');
-INSERT INTO categories (category_name) VALUES ('programming');
-INSERT INTO categories (category_name) VALUES ('marketing');
-INSERT INTO categories (category_name) VALUES ('design');
-INSERT INTO categories (category_name) VALUES ('Video/Mindset');
+INSERT INTO categories (category_name) VALUES ('경제/금융');
+INSERT INTO categories (category_name) VALUES ('라이프스타일');
+INSERT INTO categories (category_name) VALUES ('요리/베이킹');
+INSERT INTO categories (category_name) VALUES ('운동/헬스');
+INSERT INTO categories (category_name) VALUES ('사진/영상');
+INSERT INTO categories (category_name) VALUES ('프로그래밍');
+INSERT INTO categories (category_name) VALUES ('마케팅');
+INSERT INTO categories (category_name) VALUES ('디자인');
+INSERT INTO categories (category_name) VALUES ('명상/마인드셋');
+INSERT INTO categories (category_name) VALUES ('어학/외국어');
 
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (1, 567, 'IT 제목1', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/+thumbnail/test-thumnail.png');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (1, 600, 'IT 제목2', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/+thumbnail/test-thumnail.png');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (1, 720, 'IT 제목3', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/+thumbnail/test-thumnail2.png');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (1, 480, 'IT 제목4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/+thumbnail/test-thumnail.png');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (2, 1020, '경제 제목1', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', '');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (2, 480, '경제 제목2', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', '');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (2, 1260, '경제 제목3', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', '');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (2, 300, '경제 제목4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', '');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (3, 420, '일상 제목1', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', '');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (3, 600, '일상 제목2', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', '');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (3, 480, '일상 제목3', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', '');
-INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (3, 840, '일상 제목4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/example.mp4', '');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (1, 154, '[핫클립] IT 기업에서는 어떤 업무를 할까', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/[핫클립]+IT+기업에서는+어떤+업무를+할까_+_+YTN+사이언스.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/[핫클립]+IT+기업에서는+어떤+업무를+할까_+_+YTN+사이언스.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (1, 900, '평생 써먹는 코딩 공부 순서, 코딩 고수는 보지 마세요', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/평생+써먹는+코딩+공부+순서%2C+코딩+고수는+보지+마세요.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/평생+써먹는+코딩+공부+순서%2C+코딩+고수는+보지+마세요.png');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (2, 1106, '모르면 평생 후회하는 비트코인 쌩기초 개념 총정리', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/economy/모르면+평생+후회하는+비트코인+쌩기초+개념+총정리.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/economy/모르면+평생+후회하는+비트코인+쌩기초+개념+총정리.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (2, 442, '경제초보, 재테크 초보를 위한 필수 상식 7분 요약', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/economy/경제초보%2C+재테크+초보를+위한+필수+상식+7분+요약.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/economy/경제초보%2C+재테크+초보를+위한+필수+상식+7분+요약.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (2, 672, '주린이라면 투자할 때 이것만 알아도 리스크를 줄일 수 있음', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/economy/주린이라면+투자할+때+이것만+알아도+리스크를+줄일+수+있음.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/economy/주린이라면+투자할+때+이것만+알아도+리스크를+줄일+수+있음.png');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (3, 572, '내일이 바뀌는 시간 관리법', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/lifestyle/내일이+바뀌는+시간+관리법.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/lifestyle/내일이+바뀌는+시간+관리법.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (3, 1272, '뇌과학으로 입증된 도파민 중독 이용한 공부 습관 만드는 법', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/lifestyle/뇌과학으로+입증된+도파민+중독+이용한+공부+습관+만드는+법.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/lifestyle/뇌과학으로+입증된+도파민+중독+이용한+공부+습관+만드는+법.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (3, 728, '라이프스타일 비즈니스의 시대', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/lifestyle/라이프스타일+비즈니스의+시대.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/lifestyle/라이프스타일+비즈니스의+시대.png');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (4, 540, '네모반듯 맛있는 체크쿠키 만들기', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/네모반듯+맛있는+체크쿠키+만들기.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/네모반듯+맛있는+체크쿠키+만들기.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (4, 274, '오레오 체크쿠키 (재료 3가지, 초간단)', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/오레오+체크쿠키+(재료+3가지%2C+초간단).mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/오레오+체크쿠키+(재료+3가지%2C+초간단).png');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (5, 1047, '비만 전문의가 말하는 효과적인 다이어트 방법', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/health/비만+전문의가+말하는+효과적인+다이어트+방법.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/health/비만+전문의가+말하는+효과적인+다이어트+방법.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (5, 1171, '잘 먹고, 잘 자고, 오래도록 활기차게 살기 위한 특급 비법', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/health/잘+먹고%2C+잘+자고%2C+오래도록+활기차게+살기+위한+특급+비법.mp4', 'https://s3.console.aws.amazon.com/s3/object/a-minute-society?region=ap-northeast-2&bucketType=general&prefix=videos/health/잘+먹고%2C+잘+자고%2C+오래도록+활기차게+살기+위한+특급+비법.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (5, 651, '헬스초보자가 이 순서대로 운동하면, 무조건 벌크업됩니다', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/health/헬스초보자가+이+순서대로+운동하면%2C+무조건+벌크업됩니다.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/health/헬스초보자가+이+순서대로+운동하면%2C+무조건+벌크업됩니다.png');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (6, 295, '2024년 입문용 카메라 추천 Top5', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/photo/2024년+입문용+카메라+추천+Top5.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/photo/2024년+입문용+카메라+추천+Top5.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (6, 588, '누구나 사진을 찍는데, 어떤 사진이 특별한 걸까', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/photo/누구나+사진을+찍는데%2C+어떤+사진이+특별한+걸까.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/photo/누구나+사진을+찍는데%2C+어떤+사진이+특별한+걸까.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (6, 760, '방송국 편집자들 입 떡 벌어지는 미친 속도로 영상 편집해 주는 기술', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/photo/방송국+편집자들+입+떡+벌어지는+미친+속도로+영상+편집해+주는+기술.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/photo/방송국+편집자들+입+떡+벌어지는+미친+속도로+영상+편집해+주는+기술.png');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (7, 404, '초등학생도 이해하는 프로그래밍 기초', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/초등학생도+이해하는+프로그래밍+기초.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/초등학생도+이해하는+프로그래밍+기초.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (7, 529, '콜백함수가 뭔지 한국어로 쉽게 설명하는 영상', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/콜백함수가+뭔지+한국어로+쉽게+설명하는+영상.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/콜백함수가+뭔지+한국어로+쉽게+설명하는+영상.png');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (8, 1080, '2024년 마케팅 흐름은 이렇게 변하고 있습니다', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/marketing/2024년+마케팅+흐름은+이렇게+변하고+있습니다.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/marketing/2024년+마케팅+흐름은+이렇게+변하고+있습니다.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (8, 405, '마케팅 공부법 5가지만 따라하세요', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/marketing/마케팅+공부법+5가지만+따라하세요.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/marketing/마케팅+공부법+5가지만+따라하세요.png');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (9, 637, '2024 디자인 트렌드 살펴보기 디자이너들 집중', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/design/2024+디자인+트렌드+살펴보기+디자이너들+집중.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/design/2024+디자인+트렌드+살펴보기+디자이너들+집중.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (9, 918, '디자인 현실적인 고민 무엇이든 물어보세요', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/design/디자인+현실적인+고민+무엇이든+물어보세요.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/design/디자인+현실적인+고민+무엇이든+물어보세요.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (9, 493, '직장선배가 알려주는 디자인 취업', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/design/직장선배가+알려주는+디자인+취업.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/design/직장선배가+알려주는+디자인+취업.png');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (10, 453, '5분 아침명상', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/mindset/5분+아침명상.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/mindset/5분+아침명상.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (10, 432, '5분 호흡명상', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/mindset/5분+호흡명상.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/mindset/5분+호흡명상.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (10, 601, '무려 30년을 바쳐 찾아낸 반드시 성공하는 3가지 문장', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/mindset/무려+30년을+바쳐+찾아낸+반드시+성공하는+3가지+문장.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/mindset/무려+30년을+바쳐+찾아낸+반드시+성공하는+3가지+문장.png');
+
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (11, 767, '다 큰 성인이 혼자서 6개월 만에 영어공부 성공하는 3가지 방법', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/language/다+큰+성인이+혼자서+6개월+만에+영어공부+성공하는+3가지+방법.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/language/다+큰+성인이+혼자서+6개월+만에+영어공부+성공하는+3가지+방법.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (11, 897, '언어는 학습하는 것이 아니라 습득하는 것입니다', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/language/언어는+학습하는+것이+아니라+습득하는+것입니다.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/language/언어는+학습하는+것이+아니라+습득하는+것입니다.png');
+INSERT INTO videos (category_id, run_time, video_title, url, video_thumbnail) VALUES (11, 793, '주토피아 - 영화로 배우는 영어공부', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/language/주토피아+-+영화로+배우는+영어공부.mp4', 'https://a-minute-society.s3.ap-northeast-2.amazonaws.com/videos/language/주토피아+-+영화로+배우는+영어공부.png');
+
+
 
 INSERT INTO users (user_id, user_name, user_pw) VALUES ('hello', 'a', '1234');
