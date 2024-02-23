0763cbffa68d9ec6d4eaffd2376436653ce16eb2
MinseoKangQ <kms02171@naver.com>
2024-02-23 15:48:39 +0900

    :memo: Controller swagger 문서 작성

--- src/main/java/com/project/aminutesociety/scrap/controller/ScrapController.java
+++ src/main/java/com/project/aminutesociety/scrap/controller/ScrapController.java
@@ -2,10 +2,13 @@ package com.project.aminutesociety.scrap.controller;
 
 import com.project.aminutesociety.scrap.service.ScrapServiceImpl;
 import com.project.aminutesociety.util.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Scrap", description = "스크립 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/scrap")
@@ -13,18 +16,21 @@ public class ScrapController {
 
     private final ScrapServiceImpl scrapService;
 
+    @Operation(summary = "스크랩")
     @PostMapping("{userId}/{videoId}")
     public ResponseEntity<ApiResponse<?>> createScrap(@PathVariable("userId") String userId, @PathVariable("videoId") Long videoId) {
         ResponseEntity<ApiResponse<?>> result = scrapService.createScrap(userId, videoId);
         return result;
     }
 
+    @Operation(summary = "스크랩 취소")
     @DeleteMapping("{userId}/{videoId}")
     public ResponseEntity<ApiResponse<?>> deleteScrap(@PathVariable("userId") String userId, @PathVariable("videoId") Long videoId) {
         ResponseEntity<ApiResponse<?>> result = scrapService.deleteScrap(userId, videoId);
         return result;
     }
 
+    @Operation(summary = "스크랩 영상 조회")
     @GetMapping("{userId}")
     public ResponseEntity<ApiResponse<?>> getUserScrap(@PathVariable("userId") String userId, @RequestParam("type") String type) {
         ResponseEntity<ApiResponse<?>> result = scrapService.getUserScrap(userId, type);
--- src/main/java/com/project/aminutesociety/video/controller/VideoController.java
+++ src/main/java/com/project/aminutesociety/video/controller/VideoController.java
@@ -3,10 +3,13 @@ package com.project.aminutesociety.video.controller;
 import com.project.aminutesociety.util.response.ApiResponse;
 import com.project.aminutesociety.video.dto.EditRecommendVideo;
 import com.project.aminutesociety.video.service.VideoServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Video", description = "영상 관련 API")
 @RestController
 @RequestMapping("api/video")
 @RequiredArgsConstructor
@@ -14,20 +17,23 @@ public class VideoController {
 
     private final VideoServiceImpl videoService;
 
+    @Operation(summary = "출발지/도착지 설정 후 영상 추천")
     @GetMapping("recommend/{userId}")
     public ResponseEntity<ApiResponse<?>> recommendVideo(@PathVariable("userId") String userId, @RequestParam("time") Integer time) {
         ResponseEntity<ApiResponse<?>> result = videoService.recommendVideo(userId, time);
         return result;
     }
 
+    @Operation(summary = "추천 영상 1개 수정")
     @PutMapping("recommend/{userId}")
     public ResponseEntity<ApiResponse<?>> editRecommendVideo(@PathVariable("userId") String userId,
-                                                             @RequestParam("time") Integer time,
+                                                            @RequestParam("time") Integer time,
                                                             @RequestBody EditRecommendVideo.Req req) {
         ResponseEntity<ApiResponse<?>> result = videoService.editRecommendVideo(userId, time, req);
         return result;
     }
 
+    @Operation(summary = "홈에서 사용자 카테고리 기반의 영상 추천")
     @GetMapping("home/recommend/{userId}")
     public ResponseEntity<ApiResponse<?>> getVideoRecommend(@PathVariable("userId") String userId) {
         ResponseEntity<ApiResponse<?>> result = videoService.getVideoRecommend(userId);
